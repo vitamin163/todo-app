747a9dbeb8f81cffc8088eaa7155b8a77a55be40
Vitaliy D <vitalik.domnin@gmail.com>
2020-03-05 18:22:16 +0400

    modified package.json

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,19 @@
-import { createAction } from "redux-actions";
-import axios from "axios";
+import { createAction } from 'redux-actions';
+import axios from 'axios';
 
-export const moveTask = createAction("MOVE_TASK");
-export const moveTaskOtherColumn = createAction("MOVE_TASK_OTHER_COLUMN");
+export const moveTask = createAction('MOVE_TASK');
+export const moveTaskOtherColumn = createAction('MOVE_TASK_OTHER_COLUMN');
 
-export const addTaskSuccess = createAction("TASK_ADD_SUCCESS");
+export const addTaskSuccess = createAction('TASK_ADD_SUCCESS');
 
 export const addTask = ({ task }) => async dispatch => {
-  const response = await axios.post("http://localhost:3001/tasks", { ...task });
+  const response = await axios.post('http://localhost:3001/tasks', { ...task });
   dispatch(addTaskSuccess({ task: response.data }));
 };
 
-export const removeTaskRequest = createAction("REMOVE_TASK_REQUEST");
-export const removeTaskSuccess = createAction("REMOVE_TASK_SUCCESS");
-export const removeTaskFailure = createAction("REMOVE_TASK_FAILURE");
+export const removeTaskRequest = createAction('REMOVE_TASK_REQUEST');
+export const removeTaskSuccess = createAction('REMOVE_TASK_SUCCESS');
+export const removeTaskFailure = createAction('REMOVE_TASK_FAILURE');
 
 export const removeTask = ({ id, columnId }) => async dispatch => {
   dispatch(removeTaskRequest());
